1aedee7ccab3b464786d8c76a6ca760b6610d493
Ümit KARABACAK <umit.karabacak@hotmail.com.tr>
2024-09-02 23:36:55 +0300

    configuration chnaged

--- DynamicAutoMapper/DynamicProfile.cs
+++ DynamicAutoMapper/DynamicProfile.cs
@@ -4,16 +4,14 @@ public class DynamicProfile : Profile
 {
     public DynamicProfile()
     {
-#if DEBUG
         // BaseEntity<> ve BaseEntityViewModel<> türlerinden birinin assembly'sini alın
         var modelBaseEntityAssembly = typeof(BaseEntity<>).Assembly;
         var modelBaseEntityViewModelAssembly = typeof(BaseEntityViewModel<>).Assembly;
 
         var assemblies = new[] { modelBaseEntityAssembly, modelBaseEntityViewModelAssembly };
-#else
-        // Get all assemblies referenced by the current assembly
-        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-#endif
+
+        //// Get all assemblies referenced by the current assembly
+        ////var assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
         // Find all types derived from BaseEntity<T>
         var entityTypes = assemblies.SelectMany(a => a.GetTypes())
